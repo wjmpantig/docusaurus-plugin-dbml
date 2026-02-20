34a34b2aebbf962a7c4f13a4f7e47825ec341722
w <wjmpantig@gmail.com>
2026-02-20 16:10:16 +0800

    fix path when importing

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,6 +1,9 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import type { LoadContext, Plugin } from "@docusaurus/types";
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path'
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 export default function docusaurusPluginDbml(
   _context?: LoadContext,
   _options?: unknown
@@ -10,11 +13,11 @@ export default function docusaurusPluginDbml(
     
     getThemePath() {
       // Return the path to the theme directory relative to this file
-      return "../dist/theme";
+      return join(__dirname, 'theme');
     },
     
     getTypeScriptThemePath() {
-      return "../src/theme";
+      return join(__dirname, '../src/theme');
     },
   };
 }
--- vite.config.ts
+++ vite.config.ts
@@ -46,6 +46,8 @@ export default defineConfig({
 				"@wjmpantig/react-dbml-renderer/style.css",
 				"@xyflow/react",
 				"@xyflow/react/dist/style.css",
+				'path',
+				'url',
 			],
 			output: {
 				globals: {
